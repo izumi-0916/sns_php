d58c8148cce6c4c5c2285a8571e4001bb6b5558a
izumi <kizumi916@gmail.com>
2020-05-19 14:56:45 +0900

    Initial commit

---
+++ public_html/index.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once(__DIR__ . '/../config/config.php');
+
+$app = new MyApp\Controller\Index();
+
+$app->run();
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Home</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div id="container">
+    <form action="logout.php" method="post" id="logout">
+      <?= h($app->me()->email); ?><input type="submit" value="ログアウト">
+      <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+    </form>
+    <h1>Users <span class="fs12">(<?= count($app->getValues()->users); ?>)</span></h1>
+    <ul>
+      <?php foreach ($app->getValues()->users as $user) : ?>
+        <li><?= h($user->email); ?></li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+</body>
+</html>
